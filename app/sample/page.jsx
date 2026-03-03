336d87ddc1b295cb604d1a8cbe59fb2dc959dd5e
mohdqaseem1 <qaseemshahid198@gmail.com>
2026-03-03 21:08:44 +0530

    list-virtualization

--- app/sample/page.jsx
+++ app/sample/page.jsx
@@ -1,8 +1,28 @@
+"use client"
+import { FixedSizeList as List } from "react-window";
 
 const Sample = () =>{
+
+    const items = Array.from({length : 10000}, (_, index) => `item ${index+1}`);
+
+    const Row = ({ index, style }) => (
+    <div style={style}>
+      {items[index]}
+    </div>
+  );
+
     return(
         <div>
-            Loading sample ...
+            {/* {array.map((single) => <li>{single}</li>)} */}
+            <h3>List Virtualization here</h3>
+
+            <List
+                height={500}          // Visible area height
+//                width={500}           // Width of list
+                itemSize={80}         // Height of each row
+                itemCount={items.length}>
+                {Row}
+            </List>
         </div>
     )
 };
